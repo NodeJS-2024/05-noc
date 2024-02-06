4e42c9cb80fa9e3fbaf2e82168ad33661dea16fb
Luis Angel <angelfgdeveloper@gmail.com>
2024-02-05 20:48:44 -0600

    Logica de negocio de GetLogs

--- src/domain/datasources/log.datasource.ts
+++ src/domain/datasources/log.datasource.ts
@@ -3,5 +3,5 @@ import { LogEntity, LogSeverityLevel } from '../entities/log.entity';
 // Reglas de negocios
 export abstract class LogDataSource {
   abstract saveLog(log: LogEntity): Promise<void>;
-  abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity>;
+  abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
 }
--- src/domain/repository/log.repository.ts
+++ src/domain/repository/log.repository.ts
@@ -3,5 +3,5 @@ import { LogEntity, LogSeverityLevel } from '../entities/log.entity';
 // Es quien permite llamar a mis reglas de negocio
 export abstract class LogRepository {
   abstract saveLog(log: LogEntity): Promise<void>;
-  abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity>;
+  abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
 }
