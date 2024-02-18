be878432426363d6a5913edaf68e4a381c52c431
Luis Angel <angelfgdeveloper@gmail.com>
2024-02-18 16:51:04 -0600

    Crear y mostrar registros de logs desde mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from './config/plugins/envs.plugin';
-import { MongoDatabase } from './data/mongo-db';
+import { LogModel, MongoDatabase } from './data/mongo-db';
 import { Server } from './presentation/server';
 
 (async() => {
@@ -12,6 +12,24 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
   });
+
+  // Coleccion => tablas
+  // Documento => registros
+
+  // Crear registro
+  // const newLog = await LogModel.create({
+  //   message: 'Test message desde Mongo',
+  //   origin: 'App.ts',
+  //   level: 'low'
+  // });
+
+  // await newLog.save();
+  // console.log(newLog);
+
+  // Obtener registros
+  // const logs = await LogModel.find();
+  // console.log(logs);
+
 
   // Server.start();
   // console.log(envs);
