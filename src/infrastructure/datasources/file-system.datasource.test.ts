c0a8ab232ef153492bee6b0ffe8070368a6f747c
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-02 19:26:21 -0600

    Pruebas completas de fileSystemDataSource

--- src/infrastructure/datasources/file-system.datasource.test.ts
+++ src/infrastructure/datasources/file-system.datasource.test.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { FileSystemDataSource } from './file-system.datasource';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 describe('Pruebas en FileSystemaDataSource.ts', () => {
 
@@ -15,14 +16,132 @@ describe('Pruebas en FileSystemaDataSource.ts', () => {
   test('should create log files if the do not exists', async () => {
 
     new FileSystemDataSource();
-    const  files = fs.readdirSync(logPath);
+    const files = fs.readdirSync(logPath);
 
     // console.log(files);
     expect(files).toEqual([ 'logs-all.log', 'logs-high.log', 'logs-medium.log' ]);
     
   });
 
-  test('should first', async () => {
+  test('should save a log in all logs logs-all.log', async () => {
+
+    const logDataSource = new FileSystemDataSource();
+
+    const log = new LogEntity({
+      message: 'test',
+      level: LogSeverityLevel.low,
+      origin: 'file-system-datasource.test.ts'
+    });
+
+    logDataSource.saveLog(log);
+    const allLogs = fs.readFileSync(`${ logPath }/logs-all.log`, 'utf-8');
+    // console.log(allLogs);
+
+    expect(allLogs).toContain(JSON.stringify(log));
+
+  });
+
+  test('should save a log in all logs logs-all.log and logs-medium.log', async () => {
+
+    const logDataSource = new FileSystemDataSource();
+
+    const log = new LogEntity({
+      message: 'test',
+      level: LogSeverityLevel.medium,
+      origin: 'file-system-datasource.test.ts'
+    });
+
+    logDataSource.saveLog(log);
+    const allLogs = fs.readFileSync(`${ logPath }/logs-all.log`, 'utf-8');
+    const mediumLogs = fs.readFileSync(`${ logPath }/logs-medium.log`, 'utf-8');
+    // console.log(allLogs);
+
+    expect(allLogs).toContain(JSON.stringify(log));
+    expect(mediumLogs).toContain(JSON.stringify(log));
+
+  });
+
+  test('should save a log in all logs logs-all.log and logs-high.log', async () => {
+
+    const logDataSource = new FileSystemDataSource();
+
+    const log = new LogEntity({
+      message: 'test',
+      level: LogSeverityLevel.high,
+      origin: 'file-system-datasource.test.ts'
+    });
+
+    logDataSource.saveLog(log);
+    const allLogs = fs.readFileSync(`${ logPath }/logs-all.log`, 'utf-8');
+    const highLogs = fs.readFileSync(`${ logPath }/logs-high.log`, 'utf-8');
+
+    expect(allLogs).toContain(JSON.stringify(log));
+    expect(highLogs).toContain(JSON.stringify(log));
+
+  });
+
+  test('should return all logs', async () => {
+
+    const logDataSource = new FileSystemDataSource();
+
+    const logLow = new LogEntity({
+      message: 'log-low',
+      level: LogSeverityLevel.low,
+      origin: 'file-system-datasource.test.ts'
+    });
+
+    const logMedium = new LogEntity({
+      message: 'log-medium',
+      level: LogSeverityLevel.medium,
+      origin: 'file-system-datasource.test.ts'
+    });
+
+    const logHigh = new LogEntity({
+      message: 'log-high',
+      level: LogSeverityLevel.high,
+      origin: 'file-system-datasource.test.ts'
+    });
+
+    await logDataSource.saveLog(logLow);
+    await logDataSource.saveLog(logMedium);
+    await logDataSource.saveLog(logHigh);
+
+    const logsLow = await logDataSource.getLogs(LogSeverityLevel.low);
+    const logsmeidum = await logDataSource.getLogs(LogSeverityLevel.medium);
+    const logsHigh = await logDataSource.getLogs(LogSeverityLevel.high);
+
+    expect(logsLow).toEqual(expect.arrayContaining([ logLow, logMedium, logHigh ]));
+    expect(logsmeidum).toEqual(expect.arrayContaining([ logMedium ]));
+    expect(logsHigh).toEqual(expect.arrayContaining([ logHigh ]));
+
+  });
+
+  test('should not throw an error if path exists', async () => {
+
+    new FileSystemDataSource();
+    new FileSystemDataSource();
+
+    expect(true).toBeTruthy();
+
+  });
+
+  test('should throw an error if severity level is not defined', async () => {
+
+    const logDataSource = new FileSystemDataSource();
+
+    // Consideralo un enum de LogSeverityLevel
+    const customSeverityLevel = 'SUPER_MEGA_HIGH' as LogSeverityLevel;
+
+    try {
+      await logDataSource.getLogs(customSeverityLevel);
+      expect(true).toBeFalsy(); // Nunca se debe de ejecutar
+    } catch (error) {
+
+      const errorString = `${ error }`;
+
+      // console.log({ errorString });
+      expect(errorString).toContain(`${ customSeverityLevel } not implemented`);
+    }
 
   });
   
