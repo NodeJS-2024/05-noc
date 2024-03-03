94e7307b0aced18a0d87e50d9e413b3a4be509ea
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-02 18:02:11 -0600

    Pruebas en SendEmailLogs

---
+++ src/domain/use-cases/email/send-email-log.test.ts
@@ -0,0 +1,63 @@
+import { LogEntity } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
+import { SendEmailLogs } from './send-email-log';
+
+describe('SendEmailLog.ts', () => {
+
+  const mockEmailService = {
+    sendEmailWithFileSystemLogs: jest.fn().mockReturnValue(true)
+  }
+
+  const mockLogRepository: LogRepository = {
+    saveLog: jest.fn(),
+    getLogs: jest.fn(),
+  }
+
+  const sendEmailLogs = new SendEmailLogs(
+    mockEmailService as any,
+    mockLogRepository
+  );
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  });
+
+  test('should call sendEmail and saveLog', async () => {
+
+    const result = await sendEmailLogs.execute('angelfgdeveloper@gmail.com');
+
+    expect( result ).toBeTruthy();
+    expect( mockEmailService.sendEmailWithFileSystemLogs ).toHaveBeenCalledTimes(1);
+    expect( mockLogRepository.saveLog ).toHaveBeenCalledWith(
+      expect.any(LogEntity)
+    );
+    expect( mockLogRepository.saveLog ).toHaveBeenCalledWith({
+      createdAt: expect.any(Date), 
+      level: "low", 
+      message: "Log email sent", 
+      origin: "send-email-logs.ts"
+    });
+
+  });
+
+  test('should log in case of error', async () => {
+
+    mockEmailService.sendEmailWithFileSystemLogs.mockReturnValue(false);
+
+    const result = await sendEmailLogs.execute('angelfgdeveloper@gmail.com');
+
+    expect( result ).toBeFalsy();
+    expect( mockEmailService.sendEmailWithFileSystemLogs ).toHaveBeenCalledTimes(1);
+    expect( mockLogRepository.saveLog ).toHaveBeenCalledWith(
+      expect.any(LogEntity)
+    );
+    expect( mockLogRepository.saveLog ).toHaveBeenCalledWith({
+      createdAt: expect.any(Date), 
+      level: "high", 
+      message: "Error: Email log not sent", 
+      origin: "send-email-logs.ts"
+    });
+
+  });
+  
+});
